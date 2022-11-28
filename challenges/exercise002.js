be115c7e928de46e2a682b4a245bf0304c758398
Tom <admin@Admins-MBP.lan>
2022-11-28 15:08:05 +0000

    Excercises 002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,49 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+
+  if (person.city === "Manchester") {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+
+  const bus = 40;
+  const minBuses = Math.ceil(people / bus)
+
+  return minBuses;
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+
+  let sheep = 0;
+
+  arr.forEach(animal => {
+    if (animal === "sheep") { sheep++; }
+  });
+
+  return sheep;
 }
 
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+
+  const re = new RegExp('^[a-zA-Z][0-9]*$');
+
+  let postcodeSplit = person.address.postCode.split(' ')[0];
+
+  let result = re.test(postcodeSplit);
+
+  return result;
 }
