e0f233cefb90437ebd6a9772b236580b82da8ec4
Tom <admin@Admins-MBP.lan>
2022-12-02 11:54:23 +0000

    Exercises 006 with tests

--- challenges/exercise006.js
+++ challenges/exercise006.js
@@ -6,6 +6,22 @@
  */
 export const sumMultiples = (arr) => {
   if (arr === undefined) throw new Error("arr is required");
+
+  const newArr = [];
+
+  arr.forEach(m => {
+    if ((m % 3) === 0 || (m % 5) === 0) {
+      newArr.push(m);
+    }
+  });
+
+  if (newArr.length > 0) {
+    return newArr.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+    );
+  }
+
+  return 0;
 };
 
 /**
@@ -15,6 +31,16 @@ export const sumMultiples = (arr) => {
  */
 export const isValidDNA = (str) => {
   if (str === undefined) throw new Error("str is required");
+
+  let regexResultArr = [];
+
+  regexResultArr = str.match(/[CGTA]/g);
+
+  if (regexResultArr) {
+    return regexResultArr.length === str.length;
+  }
+
+  return false;
 };
 
 /**
@@ -24,6 +50,30 @@ export const isValidDNA = (str) => {
  */
 export const getComplementaryDNA = (str) => {
   if (str === undefined) throw new Error("str is required");
+
+  const strArr = str.split('');
+
+  const newStrArr = []
+
+  strArr.forEach(letter => {
+    if (letter === 'T') {
+      newStrArr.push('A')
+    }
+
+    if (letter === 'A') {
+      newStrArr.push('T')
+    }
+
+    if (letter === 'C') {
+      newStrArr.push('G')
+    }
+
+    if (letter === 'G') {
+      newStrArr.push('C')
+    }
+  });
+
+  return newStrArr.join('');
 };
 
 /**
@@ -33,7 +83,23 @@ export const getComplementaryDNA = (str) => {
  */
 export const isItPrime = (n) => {
   if (n === undefined) throw new Error("n is required");
-};
+
+  if (n === 1 || n < 1) {
+    return false;
+  }
+
+  if (n === 2) {
+    return true;
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
 
 /**
  * This function should receive a number and return an array of n arrays, each filled with n items. The parameter "fill" should be used as the filler of the arrays. For example, given parameters 3 and "foo" the resulting matrix should be:
@@ -49,6 +115,19 @@ export const isItPrime = (n) => {
 export const createMatrix = (n, fill) => {
   if (n === undefined) throw new Error("n is required");
   if (fill === undefined) throw new Error("fill is required");
+
+  const matrix = []
+  const fullMatrix = []
+
+  for (let i = 0; n > i; i++) {
+    matrix.push(fill);
+  }
+
+  matrix.forEach(() => {
+    fullMatrix.push(matrix);
+  })
+
+  return fullMatrix;
 };
 
 /**
@@ -66,4 +145,27 @@ export const createMatrix = (n, fill) => {
 export const areWeCovered = (staff, day) => {
   if (staff === undefined) throw new Error("staff is required");
   if (day === undefined) throw new Error("day is required");
+
+
+  const rota = staff.map((m) => m.rota);
+  const days = {};
+  day = day.toLowerCase();
+
+  rota.map((s) => {
+    s.map((currentDay) => {
+      currentDay = currentDay.toLowerCase();
+
+      if (days[currentDay] === undefined) {
+        days[currentDay] = 1;
+      } else {
+        days[currentDay] += 1;
+      }
+    })
+  })
+
+  if (days[day] && days[day] >= 3) {
+    return true;
+  }
+
+  return false;
 };
---
+++ test/exercise006.test.js
@@ -0,0 +1,152 @@
+import {
+  sumMultiples,
+  isValidDNA,
+  getComplementaryDNA,
+  isItPrime,
+  createMatrix,
+  areWeCovered,
+} from "../challenges/exercise006";
+
+describe("sumMultiples", () => {
+    
+  /**
+   * This function will receive an array of numbers and should return the sum
+   * of any numbers which are a multiple of 3 or 5
+   * @param {Array} arr
+   * @returns {Number}
+   */
+
+  test("returns the sum of any numbers which are a multiple of 3 or 5", () => {
+    expect(sumMultiples([1, 2, 3, 4, 5, 6, 7, 8, 9])).toBe(23);
+    expect(sumMultiples([11, 22, 33, 44, 55, 66, 77, 88, 99])).toBe(253);
+  });
+
+  test("returns 0 if no numbers which are a multiple of 3 or 5 are passed in", () => {
+    expect(sumMultiples([1, 2, 4, 7, 8, 11, 13])).toBe(0);
+  });
+
+});
+
+describe("isValidDNA", () => {
+
+  /**
+   * This function will receive a string of characters and should return true/false depending on whether it is a valid DNA string. A valid DNA string may contain characters C, G, T or A only.
+   * @param {String} str
+   * @returns {Boolean}
+   */
+
+  test("returns true if a valid DNA (CGTA) string is passed in", () => {
+    expect(isValidDNA('CGTA')).toBe(true);
+    expect(isValidDNA('CGTAGTAGGATC')).toBe(true);
+    expect(isValidDNA('CCCCCCCCCGGGGGGGGTTTTTTTAAAAAAAAA')).toBe(true);
+  });
+
+  test("returns false if an invalid DNA (!CGTA) string is passed in", () => {
+    expect(isValidDNA('test')).toBe(false);
+    expect(isValidDNA('xCGTAGTAGGATC')).toBe(false);
+    expect(isValidDNA('CCCCCCCCC GGGGGGGG TTTTTTT AAAAAAAAA')).toBe(false);
+  });
+});
+
+describe("getComplementaryDNA", () => {
+
+  /**
+   * This function will receive a valid DNA string (see above) and should return a string of the complementary base pairs. In DNA, T always pairs with A, and C always pairs with G. So a string of "ACTG" would have a complementary DNA string of "TGAC".
+   * @param {String} str
+   * @returns {String}
+   */
+  test("returns a string of the complementary base pairs (T <=> A, C <=> G)", () => {
+    expect(getComplementaryDNA('CGTA')).toBe("GCAT");
+    expect(getComplementaryDNA('CCCCGGGGTTTTAAAA')).toBe("GGGGCCCCAAAATTTT");
+  });
+});
+
+describe("isItPrime", () => {
+
+/**
+ * This function should receive a number and return true/false depending on whether it is a prime number or not. A prime number is a number that can only be divided evenly by 1 and itself (for example, 7)
+ * @param {Number} n
+ * @returns {Boolean}
+ */
+
+
+  test("returns true if the number is a prime number", () => {
+    expect(isItPrime(2)).toBe(true);
+    expect(isItPrime(3)).toBe(true);
+    expect(isItPrime(5)).toBe(true);
+    expect(isItPrime(29)).toBe(true);
+    expect(isItPrime(997)).toBe(true);
+  });
+
+  test("returns false if the number is not a prime number", () => {
+    expect(isItPrime(1)).toBe(false);
+    expect(isItPrime(4)).toBe(false);
+    expect(isItPrime(6)).toBe(false);
+    expect(isItPrime(30)).toBe(false);
+    expect(isItPrime(1000)).toBe(false);
+  });
+});
+
+describe("createMatrix", () => {
+
+/**
+ * This function should receive a number and return an array of n arrays, each filled with n items. The parameter "fill" should be used as the filler of the arrays. For example, given parameters 3 and "foo" the resulting matrix should be:
+ * [
+ *   ["foo", "foo", "foo"],
+ *   ["foo", "foo", "foo"],
+ *   ["foo", "foo", "foo"]
+ * ]
+ * @param {Number} n
+ * @param {Any} fill
+ * @returns {Array}
+ */
+
+  test("returns an array of n arrays, each filled with n item", () => {
+    expect(createMatrix(3, 'foo')).toEqual([
+      ["foo", "foo", "foo"],
+      ["foo", "foo", "foo"],
+      ["foo", "foo", "foo"]
+    ]);
+
+    expect(createMatrix(5, 1)).toEqual([
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1]
+    ]);
+  });
+});
+
+
+describe("areWeCovered", () => {
+
+  /**
+ * This function takes an array of staff objects in the format:
+ * [
+ *  { name: "Sally", rota: ["Monday", "Tuesday", "Friday"] },
+ *  { name: "Pedro", rota: ["Saturday", "Sunday", "Tuesday", "Wednesday"] },
+ *  ...etc
+ * ]
+ * and a day of the week. For the café to run successfully, at least 3 staff members are required per day. The function should return true/false depending on whether there are enough staff scheduled for the given day.
+ * @param {Array} staff
+ * @param {String} day
+ * @returns {Boolean}
+ */
+
+  test("returns true if there are at least 3 staff memebers on the required day", () => {
+    expect(areWeCovered([
+      { name: "Sally", rota: ["Monday", "Tuesday", "Friday"] },
+      { name: "Pedro", rota: ["Saturday", "Sunday", "Tuesday", "Wednesday"] },
+      { name: "Steve", rota: ["Monday", "Sunday", "Tuesday", "Wednesday"] },
+    ], 'Tuesday')).toBe(true);
+  });
+
+  test("returns false if there are less then 3 staff memebers on the required day", () => {
+    expect(areWeCovered([
+      { name: "Sally", rota: ["Monday", "Tuesday", "Friday"] },
+      { name: "Pedro", rota: ["Saturday", "Wednesday"] },
+      { name: "Steve", rota: ["Monday", "Sunday", "Wednesday"] },
+    ], 'Tuesday')).toBe(false);
+  });
+});
